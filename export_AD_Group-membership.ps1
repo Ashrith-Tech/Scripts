20b378285b993b2c3030dc2da516cf1c2238a8c2
Ashrith-Tech <ashrith.dasyam@gmail.com>
2026-04-27 02:43:47 -0500

    Add files via upload

---
+++ export_AD_Group-membership.ps1
@@ -0,0 +1,22 @@
+﻿$groupName = "ServerSoftwareUpdatesMfgTier_Sun-Morning_Ent_AppGG"
+
+$outputCsvPath = "C:\Users\gkota\OneDrive - Brunswick Corporation\Desktop\Patching Sheet\MfgTier_Sun-Morn.csv"
+
+Import-Module ActiveDirectory
+
+$group = Get-ADGroup -Identity $groupName
+$groupMembers = Get-ADGroupMember -Identity $group -Recursive | Where-Object { $_.objectClass -eq 'computer' }
+
+$computerDetails = @()
+
+foreach ($computer in $groupMembers) {
+    $computerInfo = Get-ADComputer -Identity $computer.DistinguishedName -Properties CanonicalName
+    $computerDetails += [PSCustomObject]@{
+        Name         = $computerInfo.Name
+        CanonicalName = $computerInfo.CanonicalName
+    }
+}
+
+$computerDetails | Export-Csv -Path $outputCsvPath -NoTypeInformation
+
+Write-Output "Computer details exported to $outputCsvPath"
